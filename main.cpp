01dd30e0e14bf57a35194f2ac071f44e6b4ed69b
EmmanuelPhillips <emmanuelphillips33@gmail.com>
2026-06-04 11:56:30 +0100

    Wrote insert and remove functions.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Entity {
 
 class Hashmap {
 private:
-  int m_size{10};
+  const int m_size{10};
   std::vector<std::list<std::pair<std::string, Entity>>> m_table;
 
 public:
@@ -35,6 +35,33 @@ public:
       }
     }
     return false;
+  }
+
+  void insert(std::string_view key, const Entity &value) {
+    int index{hash(key)};
+    auto &bucket{m_table[index]};
+    for (auto &pair : bucket) {
+      if (pair.first == key) {
+        std::cout
+            << "Key already exists. Replacing old value with new value.\n";
+        pair.second = value;
+        return;
+      }
+    }
+    bucket.emplace_back(key, value);
+  }
+
+  void remove(std::string_view key) {
+    int index{hash(key)};
+    auto &bucket{m_table[index]};
+
+    for (auto i{bucket.begin()}; i != bucket.end(); ++i) {
+      if (i->first == key) {
+        bucket.erase(i);
+        return;
+      }
+    }
+    std::cout << "Didn't erase anything. No matching key.\n";
   }
 };
 
